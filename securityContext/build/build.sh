1dfddf494c0b950d6103e644bd63aa77678008f6
currycan <ansandyzzt@gmail.com>
2024-05-23 12:02:10 +0800

    demo

--- securityContext/build/build.sh
+++ securityContext/build/build.sh
@@ -5,3 +5,10 @@ docker build -t currycan/stress-app:0.0.5 .
 
 docker push currycan/stress-app:0.0.5
 docker push currycan/stress-app:0.0.5-apline
+
+
+docker buildx build --platform=linux/arm64 -t currycan/stress-app:0.0.6-arm . -f Dockerfile-arm
+docker push currycan/stress-app:0.0.6-arm
+
+docker buildx build --platform=linux/amd64 -t currycan/stress-app:0.0.6 . -f Dockerfile-x86
+docker push currycan/stress-app:0.0.6
--- securityContext/build/run.sh
+++ securityContext/build/run.sh
@@ -4,10 +4,10 @@ set -eou pipefail
 
 #检查系统
 check_sys(){
-    if [[ -f /etc/redhat-release ]]; then
-        release="centos"
-    elif cat /etc/issue | grep -q -E -i "Alpine"; then
+    if cat /etc/issue | grep -q -E -i "Alpine"; then
         release="alpine"
+    else
+        release="not-alpine"
     fi
 }
 
@@ -16,20 +16,17 @@ if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
     check_sys
     # run as root
     if [ "$(id -u)" = '0' ]; then
-        # find ${JVM_LOGS} \! -user ${uid} -exec chown ${uid} '{}' +
-        # find . \! -user ${uid} -exec chown ${uid} '{}' +
-        chown -R ${uid}:${gid} ${JVM_LOGS} ${WORKDIR} /jmx/
-        if [[ "${release}" == "centos" ]]; then
-            exec /bin/tini /usr/bin/gosu ${uid} -- java ${APP_OPTS} ${JAVA_OPTS} ${JVM_ARGS} ${JVM_GC} ${JMX} -jar "${WORKDIR}/${JAR_FILE}" "$@"
-        elif [[ "${release}" == "alpine" ]]; then
+        if [[ "${release}" == "alpine" ]]; then
             exec /sbin/tini /sbin/su-exec ${uid}:${gid} java ${APP_OPTS} ${JAVA_OPTS} ${JVM_ARGS} ${JVM_GC} ${JMX} -jar "${WORKDIR}/${JAR_FILE}" "$@"
+        else
+            exec /bin/tini /usr/bin/gosu ${uid} -- java ${APP_OPTS} ${JAVA_OPTS} ${JVM_ARGS} ${JVM_GC} ${JMX} -jar "${WORKDIR}/${JAR_FILE}" "$@"
         fi
     # run as non-root
     else
-        if [[ "${release}" == "centos" ]]; then
-            exec /bin/tini -- java ${APP_OPTS} ${JAVA_OPTS} ${JVM_ARGS} ${JVM_GC} ${JMX} -jar "${WORKDIR}/${JAR_FILE}" "$@"
-        elif [[ "${release}" == "alpine" ]]; then
+        if [[ "${release}" == "alpine" ]]; then
             exec /sbin/tini java ${APP_OPTS} ${JAVA_OPTS} ${JVM_ARGS} ${JVM_GC} ${JMX} -jar "${WORKDIR}/${JAR_FILE}" "$@"
+        else
+            exec /bin/tini -- java ${APP_OPTS} ${JAVA_OPTS} ${JVM_ARGS} ${JVM_GC} ${JMX} -jar "${WORKDIR}/${JAR_FILE}" "$@"
         fi
     fi
 fi
